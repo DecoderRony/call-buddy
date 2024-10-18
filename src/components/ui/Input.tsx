fef81a6a89ed4d4f71d7121a0215b9d66632b9e3
Himanshu Jaroli <54548986+HJ1X@users.noreply.github.com>
2024-10-19 01:08:35 +0530

    fix: input element UI

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -12,11 +12,11 @@ function Input({
   ...rest
 }: Readonly<InputProps>) {
   return (
-    <div className="relative">
-      <div className={"absolute left-6 top-4 " + iconClassName}>{icon}</div>
+    <div className="h-12 relative">
+      <div className={"absolute left-6 top-3 " + iconClassName}>{icon}</div>
       <input
         className={
-          "h-full rounded-2xl pl-16 px-3 bg-neutral-700 border border-neutral-700 focus:ring-purple-700 focus:border-purple-700 " +
+          "h-full rounded-2xl pl-16 py-3 px-3 bg-neutral-700 border border-neutral-700 focus:ring-purple-700 focus:border-purple-700 " +
           (className || "")
         }
         {...rest}
