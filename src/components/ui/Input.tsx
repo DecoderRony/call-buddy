b1f07cf7391fbd6bc92c7a5876c34750c68c6fda
Himanshu Jaroli <54548986+HJ1X@users.noreply.github.com>
2024-10-25 18:40:42 +0530

    update: add forward ref to input component

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   icon?: React.ReactNode;
@@ -6,44 +6,45 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   variant: "light" | "dark";
 }
 
-function Input({
-  icon,
-  className,
-  iconClassName,
-  variant,
-  ...rest
-}: Readonly<InputProps>) {
-  const colorClasses =
-    variant === "dark"
-      ? "bg-neutral-700 border border-neutral-700 focus:ring-purple-700 focus:border-purple-700 "
-      : "bg-white text-neutral-900 border border-neutral-700 focus:ring-purple-700 focus:border-purple-700 ";
+const Input = forwardRef(
+  (
+    { icon, className, iconClassName, variant, ...rest }: Readonly<InputProps>,
+    ref: React.Ref<HTMLInputElement>
+  ) => {
+    const colorClasses =
+      variant === "dark"
+        ? "bg-neutral-700 border border-neutral-700 focus:ring-purple-700 focus:border-purple-700 "
+        : "bg-white text-neutral-900 border border-neutral-700 focus:ring-purple-700 focus:border-purple-700 ";
 
-  if (icon) {
+    if (icon) {
+      return (
+        <div className="h-12 relative">
+          <div className={"absolute left-6 top-3 " + iconClassName}>{icon}</div>
+          <input
+            ref={ref}
+            className={
+              "h-full rounded-2xl pl-16 py-3 px-3 " +
+              colorClasses +
+              (className ?? "")
+            }
+            {...rest}
+          />
+        </div>
+      );
+    }
+
     return (
-      <div className="h-12 relative">
-        <div className={"absolute left-6 top-3 " + iconClassName}>{icon}</div>
+      <div className="h-12">
         <input
+          ref={ref}
           className={
-            "h-full rounded-2xl pl-16 py-3 px-3 " +
-            colorClasses +
-            (className ?? "")
+            "h-full rounded-2xl py-3 px-5 " + colorClasses + (className ?? "")
           }
           {...rest}
         />
       </div>
     );
   }
-
-  return (
-    <div className="h-12">
-      <input
-        className={
-          "h-full rounded-2xl py-3 px-5 " + colorClasses + (className ?? "")
-        }
-        {...rest}
-      />
-    </div>
-  );
-}
+);
 
 export default Input;
