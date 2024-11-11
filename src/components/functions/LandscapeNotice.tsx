b1870b4ea06811bc82f991a3093fe9041e07101b
Himanshu Jaroli <54548986+HJ1X@users.noreply.github.com>
2024-11-11 17:54:35 +0530

    update: mobile ui for alert dialog

--- src/components/functions/LandscapeNotice.tsx
+++ src/components/functions/LandscapeNotice.tsx
@@ -12,7 +12,7 @@ function LandscapeNotice() {
     <AlertDialog defaultOpen>
       <AlertDialogContent
         onEscapeKeyDown={(event: KeyboardEvent) => event.preventDefault()}
-        className="flex justify-center items-center p-20"
+        className="flex justify-center items-center p-10 md:p-20 !w4/5"
       >
         <AlertDialogHeader>
           <div className="text-center w-full">
@@ -20,10 +20,10 @@ function LandscapeNotice() {
               size="8rem"
               className="text-gray-500 w-full"
             />
-            <AlertDialogTitle className="text-2xl text-neutral-900 font-semibold">
+            <AlertDialogTitle className="text-xl md:text-2xl text-neutral-900 font-semibold">
               Layout not supported
             </AlertDialogTitle>
-            <AlertDialogDescription className="text-neutral-900 text-sm">
+            <AlertDialogDescription className="text-neutral-900 text-xs md:text-sm">
               Please rotate your device to enjoy the best experience with the
               application.
             </AlertDialogDescription>
--- src/components/functions/NetworkDisabled.tsx
+++ src/components/functions/NetworkDisabled.tsx
@@ -12,15 +12,15 @@ const NetworkDisabled = () => {
     <AlertDialog defaultOpen>
       <AlertDialogContent
         onEscapeKeyDown={(event: KeyboardEvent) => event.preventDefault()}
-        className="flex justify-center items-center p-20"
+        className="flex justify-center items-center p-10 md:p-20 !w-4/5"
       >
         <AlertDialogHeader>
           <div className="text-center w-full">
             <MdWifiOff size="8rem" className="text-gray-500 w-full" />
-            <AlertDialogTitle className="text-2xl text-neutral-900 font-semibold">
+            <AlertDialogTitle className="text-xl md:text-2xl text-neutral-900 font-semibold">
               You’re Offline
             </AlertDialogTitle>
-            <AlertDialogDescription className="text-neutral-900 text-sm">
+            <AlertDialogDescription className="text-neutral-900 text-xs md:text-sm">
               Please check your internet connection. We'll resume our services once you are online.
             </AlertDialogDescription>
           </div>
